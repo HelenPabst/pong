11a4338ee769fa44d64092ef6762b25f8beb59f5
Helen <hlnpabst@gmail.com>
2018-10-04 23:00:42 -0700

    Player Movement

---
+++ pong/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D player;
+    float vertical;
+    public float moveSpeed;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        player = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+	}
+
+    void FixedUpdate()
+    {
+        player.velocity = new Vector2(0, vertical * moveSpeed);
+    }
+}
